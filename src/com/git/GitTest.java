21f6800c97971d358c61ac6d95e8bdb2ad171afb
username <a123456@163.com>
2019-01-08 09:45:12 +0800

    将git下修改后的工程添加到本地仓库,

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -1,7 +1,18 @@
 package com.git;
 
+import org.junit.Test;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("创建gitProject");
+    }
+    @Test
+    public void  gitTest(){
+        int a = 5000;
+        for (int i = 2; i < a; i++) {
+            if(a%i==0){
+                System.out.println(i);
+            }
+        }
     }
 }
